f95b49822de1131c06f31895da088799ee1015cc
Tristan <tristan.warren2@finance.nsw.gov.au>
2019-06-11 21:14:03 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,25 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped queen");
+
+var cardsTwo = cards[2];
+
+cardsInPlay.push(cardsTwo);
+
+console.log("User flipped a king");
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match");
+    } else {
+        alert("Sorry, try again.");
+    }
+} 
